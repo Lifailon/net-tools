0fb5e6c98bd5221173ef04c334225b9a683d12e7
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-09-14 09:32:59 +0300

    Delete lspage.sh

--- lspage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-fc=$(du -a $1 2> /dev/null | awk '{print $2}' | xargs fincore 2> /dev/null)
-echo -e "PAGE\tSIZE\tPATH"
-printf "%s\n" "${fc[@]}" | grep -wvE "0B|SIZE" | awk 'BEGIN {OFS="\t"}; {print $1,$3,$4}'
